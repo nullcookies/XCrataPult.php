5a338a5f75c20153f5741db2c583502caa4e91a4
Denis Morozov <mr.xcray@gmail.com>
2014-03-16 21:18:41 +0400

    better autoloader

--- includeme.php
+++ includeme.php
@@ -8,24 +8,31 @@
 namespace X;
 define("__XDIR__", dirname(__FILE__) . DIRECTORY_SEPARATOR);
 
-function _X_AUTOLOADER($className) {
-  if (substr($className, 0, 2) !== "X\\"){
-    return;
+function registerAutoloader($namespaceName, $rootDirectory){
+
+  if (substr($rootDirectory, -1)!=DIRECTORY_SEPARATOR){
+    $rootDirectory.=DIRECTORY_SEPARATOR;
   }
 
-  $classFullName = (string)str_replace('\\', DIRECTORY_SEPARATOR, substr($className, 2));
-  $classLastName = array_slice(explode( DIRECTORY_SEPARATOR, $className), -1);
-  if (substr(array_pop($classLastName), 0, 2)=='T_'){ //trait
-    $classFullName = str_replace("T_", "", $classFullName);
-  }
-  if (class_exists("\\X\\Debug\\Logger") && class_exists("\\X\\Tools\\Time") && class_exists("\\X\\Tools\\Validators") && class_exists("\\X\\Debug\\Tracer")){
-    \X\Debug\Logger::Add("Autoloader: " . __XDIR__ . $classFullName . '.php');
-  }
+  spl_autoload_register( function($className) use(&$namespaceName, &$rootDirectory){
+    $namespaceNameLen = strlen($namespaceName);
+    if (substr($className, 0, $namespaceNameLen+1) !== $namespaceName."\\"){
+      return;
+    }
 
-  if (file_exists(__XDIR__ . $classFullName . '.php')){
-    include (__XDIR__ . $classFullName . '.php');
-  }
+    $classFullName = (string)str_replace('\\', DIRECTORY_SEPARATOR, substr($className, 2));
+    $classLastName = array_slice(explode( DIRECTORY_SEPARATOR, $className), -1);
+    if (class_exists("\\X\\Debug\\Logger") && class_exists("\\X\\Tools\\Time") && class_exists("\\X\\Tools\\Validators") && class_exists("\\X\\Debug\\Tracer")){
+      \X\Debug\Logger::Add("Autoloader: " . $rootDirectory . $classFullName . '.php...');
+    }
+
+    if (file_exists( $rootDirectory . $classFullName . '.php')){
+      include ( $rootDirectory . $classFullName . '.php');
+      \X\Debug\Logger::Add("Autoloader: " . $rootDirectory . $classFullName . '.php... Ok!');
+    }else{
+      \X\Debug\Logger::Add("Autoloader: " . $rootDirectory . $classFullName . '.php... FAIL!');
+    }
+  });
 }
 
-spl_autoload_register("\\X\\_X_AUTOLOADER");
-\X\Debug\Logger::Add("Autoloader: registered");
+registerAutoloader("X", __XDIR__);
