00b401679c8916310b48e0da1d3d9b86c29e26ce
Denis Morozov <mr.xcray@gmail.com>
2014-04-10 01:44:12 +0400

    cosmetic

--- Data/DB/Collection.php
+++ Data/DB/Collection.php
@@ -45,13 +45,11 @@ class Collection extends \ArrayObject
     $this->eof = ($this->count == 0);
   }
 
-  public function __destruct()
-  {
+  public function __destruct(){
     $this->driver->freeResource($this->res);
   }
 
-  public function row($num = null)
-  {
+  public function row($num = null){
     if ($num === null){
       $num = $this->row;
     }
@@ -78,27 +76,25 @@ class Collection extends \ArrayObject
     return $this->instantiator!==null ? call_user_func($this->instantiator, $this->lastRow) : $this->lastRow;
   }
 
-  public function reset()
-  {
+  public function reset(){
     $this->row = 0;
     $this->lastRow=null;
     $this->eof = ($this->count==0);
   }
 
-  public function next()
-  {
-    if ($this->lastRow === null)
+  public function next(){
+    if ($this->lastRow === null){
       $n = $this->row;
-    else
+    }else{
       $n = $this->row + 1;
-
+    }
     return $this->row($n);
   }
 
-  public function position($num=null)
-  {
-    if ($num===null)
+  public function position($num=null){
+    if ($num===null){
       return $this->row;
+    }
 
     if ($num >= $this->count) {
       $this->eof = true;
@@ -113,145 +109,118 @@ class Collection extends \ArrayObject
     return $this->row=$num;
   }
 
-  public function current()
-  {
+  public function current(){
     return $this->row($this->row);
   }
 
-  public function prev()
-  {
+  public function prev(){
     return $this->row($this->row - 1);
   }
 
-  public function first()
-  {
+  public function first(){
     return $this->row(0);
   }
 
-  public function last()
-  {
+  public function last(){
     return $this->row($this->count - 1);
   }
 
-  public function EOF()
-  {
+  public function EOF(){
     return $this->eof;
   }
 
-  public function size()
-  {
+  public function size(){
     return $this->count;
   }
 
-  public function offsetExists($index)
-  {
-    echo __METHOD__;
-    if (is_string($index))
+  public function offsetExists($index){
+    if (is_string($index)){
       return false;
+    }
     $index = intval($index);
     return $index >= 0 && $index < $this->count;
   }
 
-  public function offsetGet($index)
-  {
-    echo __METHOD__;
+  public function offsetGet($index){
     return $this->Row($index);
   }
 
-  public function offsetSet($index, $newval)
-  {
+  public function offsetSet($index, $newval){
     throw new \BadMethodCallException("Collection: This method is not allowed (".__METHOD__.")");
   }
 
-  public function offsetUnset($index)
-  {
+  public function offsetUnset($index){
     throw new \BadMethodCallException("Collection: This method is not allowed (".__METHOD__.")");
   }
 
-  public function append($value)
-  {
+  public function append($value){
     throw new \BadMethodCallException("Collection: This method is not allowed (".__METHOD__.")");
   }
 
-  public function getArrayCopy()
-  {
+  public function getArrayCopy(){
     throw new \BadMethodCallException("Collection: This method is not allowed (".__METHOD__.")");
   }
 
-  public function count()
-  {
+  public function count(){
     return $this->count;
   }
 
-  public function getFlags()
-  {
+  public function getFlags(){
     throw new \BadMethodCallException("Collection: This method is not allowed (".__METHOD__.")");
   }
 
-  public function setFlags($flags)
-  {
+  public function setFlags($flags){
     throw new \BadMethodCallException("Collection: This method is not allowed (".__METHOD__.")");
   }
 
-  public function asort()
-  {
+  public function asort(){
     throw new \BadMethodCallException("Collection: This method is not allowed (".__METHOD__.")");
   }
 
-  public function ksort()
-  {
+  public function ksort(){
     throw new \BadMethodCallException("Collection: This method is not allowed (".__METHOD__.")");
   }
 
-  public function uasort($cmp_function)
-  {
+  public function uasort($cmp_function){
     throw new \BadMethodCallException("Collection: This method is not allowed (".__METHOD__.")");
   }
 
-  public function uksort($cmp_function)
-  {
+  public function uksort($cmp_function){
     throw new \BadMethodCallException("Collection: This method is not allowed (".__METHOD__.")");
   }
 
-  public function natsort()
-  {
+  public function natsort(){
     throw new \BadMethodCallException("Collection: This method is not allowed (".__METHOD__.")");
   }
 
-  public function natcasesort()
-  {
+  public function natcasesort(){
     throw new \BadMethodCallException("Collection: This method is not allowed (".__METHOD__.")");
   }
 
-  public function unserialize($serialized)
-  {
+  public function unserialize($serialized){
     throw new \BadMethodCallException("Collection: This method is not allowed (".__METHOD__.")");
   }
 
-  public function serialize()
-  {
+  public function serialize(){
     throw new \BadMethodCallException("Collection: This method is not allowed (".__METHOD__.")");
   }
 
-  public function getIterator()
-  {
-    if (!$this->iterator)
+  public function getIterator(){
+    if (!$this->iterator){
       $this->iterator = new Iterator($this);
+    }
     return $this->iterator;
   }
 
-  public function exchangeArray($input)
-  {
+  public function exchangeArray($input){
     throw new \BadMethodCallException("Collection: This method is not allowed (".__METHOD__.")");
   }
 
-  public function setIteratorClass($iterator_class)
-  {
+  public function setIteratorClass($iterator_class){
     throw new \BadMethodCallException("Collection: This method is not allowed (".__METHOD__.")");
   }
 
-  public function getIteratorClass()
-  {
+  public function getIteratorClass(){
     return "\\X\\Data\\DB\\Iterator";
   }
 }
