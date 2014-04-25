1da5beac2a166fab0b5712e0cfd7f683f73909e6
Denis Morozov <mr.xcray@gmail.com>
2014-04-26 01:03:46 +0400

    +afi init

---
+++ 3rdparty/Afigenius/Autoloader.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by mr.xcray in the name of magic
+ *
+ * Date: 27.04.14
+ * Time: 0:24
+ * 
+ * Made to be used with XCrataPult.php
+ */
+
+if (class_exists("X\\X")){
+  define(__AFI_DIR__, dirname(__FILE__).'/' );
+  \X\registerAutoloader("Afi", __AFI_DIR__);
+}
