ce10691d64db1142d30157bd51ca5e51b91af887
Denis Morozov <mr.xcray@gmail.com>
2014-05-03 13:49:46 +0400

    minus 24h for cookie. Just to be sure

--- Data/Session.php
+++ Data/Session.php
@@ -31,7 +31,7 @@ class Session {
     $_SESSION = [];
     if (ini_get("session.use_cookies")) {
       $params = session_get_cookie_params();
-      setcookie(session_name(), '', time() - 42000,
+      setcookie(session_name(), '', time() - 86400,
         $params["path"], $params["domain"],
         $params["secure"], $params["httponly"]
       );
