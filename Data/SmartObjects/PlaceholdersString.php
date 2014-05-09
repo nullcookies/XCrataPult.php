15a98071ff35693fa713c8ee35c6fac1391fd394
Denis Morozov <mr.xcray@gmail.com>
2014-05-09 14:52:29 +0400

    +PlaceholdersString

---
+++ Data/SmartObjects/PlaceholdersString.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by mr.xcray in the name of magic
+ *
+ * Date: 09.05.14
+ * Time: 14:35
+ */
+
+namespace X\Data\SmartObjects;
+
+class PlaceholdersString {
+  private $expression;
+  public function __construct($expression, $defaults=[]){
+    $this->expression = $expression;
+  }
+
+  public function render(){
+    $numargs = func_num_args();
+    $string = $this->expression;
+    if ($numargs == 1 && is_array($fields = func_get_arg(0))) {
+      foreach($fields as $key=>$val){
+        $string = str_replace("%".$key, $val, $string);
+      }
+    }else{
+      $arg_list = func_get_args();
+      for ($i = 0; $i < $numargs; $i++) {
+        $string = str_replace("%".($i+1), $arg_list[$i], $string);
+      }
+    }
+    return $string;
+  }
+} 
