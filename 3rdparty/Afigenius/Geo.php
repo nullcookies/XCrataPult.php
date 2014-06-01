75d7b92949c155650dcb4531425c544fa3a81722
Denis Morozov <mr.xcray@gmail.com>
2014-06-02 02:22:15 +0400

    APIv2 support

--- 3rdparty/Afigenius/Geo.php
+++ 3rdparty/Afigenius/Geo.php
@@ -2,6 +2,9 @@
 /**
  * Created by mr.xcray in the name of magic
  *
+ * For documentation please refer to http://data.afigenius.ru/
+ * Thank you!
+ *
  * Date: 26.04.14
  * Time: 1:05
  */
@@ -9,37 +12,15 @@
 namespace Afi;
 
 class Geo {
-  public static function getCityNameById($id, $lang='ru'){
-    $id = intval($id);
-    $uri="geo/by-cityid/".$id;
-    $fields=["city_".$lang];
-    return Base::request($uri, [], $fields)["city_".$lang];
-  }
-
-  public static function getCountryDataByName($name){
-    $name = trim($name);
-    $uri="geo/by-countryname/".urlencode($name);
-    return Base::request($uri);
-  }
-
-  public static function getCountryDataById($id){
-    $id = trim($id);
-    $uri="geo/by-countryid/".$id;
-    return Base::request($uri);
-  }
-
-  public static function getCountryNameById($id, $lang='ru'){
-    return self::getCountryDataById($id)["country_".$lang];
-  }
-
-  public static function getCountryList(){
-    $uri = "geo/countrylist/";
-    return Base::request($uri);
-  }
-
-  public static function getCityListByCountryId($id){
-    $id = intval($id);
-    $uri = "geo/citylist-by-countryid/".$id;
-    return Base::request($uri)["cities"];
+  /**
+   * Returns array of Cities which has $name as part of their names in any language
+   *
+   * @param $name - part or full name of cities
+   * @param array $fields - fields you need to receive (leave empty to receive everything)
+   * @return array - list of cities
+   */
+  public static function citylistByCityname($name, $fields=[], $limit=10){
+    $uri="geo/citylist-by-cityname/".urlencode($name);
+    return Base::request($uri, [], $fields, $limit);
   }
 } 
