a00cb42d039711c8c3ed914f45eaac647aa412d3
Denis Morozov <mr.xcray@gmail.com>
2014-07-29 13:23:30 +0400

    better typograf

--- 3rdparty/Typograf/Typograf.php
+++ 3rdparty/Typograf/Typograf.php
@@ -10,22 +10,15 @@ namespace Typograf;
 
 class Typograf {
   public static function process($text){
-    $buf = $text;
-    $url = 'http://www.typograf.ru/webservice/';
-    $data = array('text' => $text, 'chr'=>'UTF-8');
+    $remoteTypograf = new RemoteTypograf();
 
-    $options = array(
-      'http' => array(
-        'header'  => "Content-type: application/x-www-form-urlencoded",
-        'method'  => 'POST',
-        'content' => http_build_query($data),
-      ),
-    );
-    $context  = stream_context_create($options);
-    $buf = file_get_contents($url, false, $context);
-    if (!$buf){
-      $buf = $text;
-    }
-    return $buf;
+    $remoteTypograf->htmlEntities();
+    $remoteTypograf->br (false);
+    $remoteTypograf->p (true);
+    $remoteTypograf->nobr (3);
+    $remoteTypograf->quotA ('laquo raquo');
+    $remoteTypograf->quotB ('bdquo ldquo');
+
+    return $remoteTypograf->processText ($text);
   }
-} 
+}
