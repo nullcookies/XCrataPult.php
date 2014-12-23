219a81c0228a1f07ac17a18c70d5bf52700f5e7d
Denis Morozov <mr.xcray@gmail.com>
2014-12-23 15:45:40 +0300

    Animated GIF detection

--- Validators/Files.php
+++ Validators/Files.php
@@ -27,4 +27,38 @@ class Files extends PrivateInstantiation{
     }
     return false;
   }
+
+  public static function isAnimatedGIF($filename){
+    if (!file_exists($filename)){
+      return false;
+    }
+
+    $filecontents=file_get_contents($filename);
+
+    $str_loc=0;
+    $count=0;
+    while ($count < 2){
+      $where1=strpos($filecontents,"\x00\x21\xF9\x04",$str_loc);
+      if ($where1 === FALSE){
+        break;
+      }else{
+        $str_loc=$where1+1;
+        $where2=strpos($filecontents,"\x00\x2C",$str_loc);
+        if ($where2 === FALSE){
+          break;
+        }else{
+          if ($where1+8 == $where2){
+            $count++;
+          }
+          $str_loc=$where2+1;
+        }
+      }
+    }
+
+    if ($count > 1){
+      return(true);
+    }else{
+      return(false);
+    }
+  }
 } 
