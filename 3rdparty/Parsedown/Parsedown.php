8f24aa21a9370e44acfe7397cdc4ac93bde4c8e4
mrXCray <mr.xcray@gmail.com>
2017-02-02 21:29:36 +0300

    PHP7 fix

--- 3rdparty/Parsedown/Parsedown.php
+++ 3rdparty/Parsedown/Parsedown.php
@@ -893,7 +893,7 @@ class Parsedown
 
             if (isset($Element['handler']))
             {
-                $markup .= $this->$Element['handler']($Element['text']);
+                $markup .= $this->{$Element['handler']}($Element['text']);
             }
             else
             {
